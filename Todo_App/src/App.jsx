b33a08017520e016843fd75dfd44fcbcc102437e
sahildkun <sahilsahoo23@gmail.com>
2023-01-22 17:48:30 +0530

    responsive

--- Todo_App/src/App.jsx
+++ Todo_App/src/App.jsx
@@ -56,9 +56,9 @@ function App() {
 
   return (
 
-    <div className="mx-12 my-10 text-center flex flex-col gap-y-4">
+    <div className="mx-0 md:mx-12 my-10 text-center flex flex-col gap-y-4">
      <div>
-      <motion.p className='text-md md:text-5xl  font-serif' animate={{   }} transition={{duration:1.3}}>TODO APP</motion.p>
+      <motion.p className='text-3xl md:text-5xl  font-serif' animate={{   }} transition={{duration:1.3}}>TODO APP</motion.p>
      </div>
      <form className='space-x-3 space-y-2' onSubmit={handleSubmit}>
       <input type="text" 
@@ -69,10 +69,10 @@ function App() {
       onKeyDown={handleKey}
       id="" 
       className='border-2 border-black px-2 py-1 md:w-[30rem]'/>
-      <button type="submit" className='text-white bg-black px-2 py-1 hover:bg-gray-600' onClick={handleSubmit} >ADD</button>
+      <button type="submit" className='text-white bg-black shadow-2xl px-2 py-2 hover:bg-gray-600 rounded-full' onClick={handleSubmit} >ADD</button>
      </form>
 
-     <div className='mx-auto sm:w-44 md:w-96'>
+     <div className='mx-auto w-60 md:w-96'>
       {
         todoList.length > 0 ? (
         todoList.map((elem ,key) => {
@@ -80,11 +80,11 @@ function App() {
         return (  
             <>
             
-            <motion.div className='flex flex-auto items-center   justify-between my-5 bg-gray-800 px-1 py-[0.5rem] rounded-lg' animate={{scale:1.4}} transition={{duration:1}}>
+            <motion.div className='flex flex-auto items-center   justify-between my-5 bg-gray-800 px-1 py-[0.5rem] rounded-lg border-2 border-gray-600' animate={{scale:1.4}} transition={{duration:1}}>
             <h1 key={key} className={`text-left text-xs max-w-[5rem] line-clamp-3 md:max-w-md ${elem.todoStatus ? 'text-gray-500 line-through' : 'text-white'} `}>{elem.todoData}</h1>
             <div className='space-x-1 md:space-x-4'>
-            <button onClick={() => markTodo(key)} className="bg-green-600 text-white p-[0.05rem] md:p-2 text-xs font-thin rounded-lg"> DONE</button>
-            <button onClick={() => deleteTodo(elem)} className="bg-red-600 text-white p-1 md:p-2 text-xs rounded-lg"> DELETE</button>
+            <button onClick={() => markTodo(key)} className="bg-green-600 text-white  p-1 md:p-2  text-[7px] md:text-xs font-thin rounded-lg">DONE</button>
+            <button onClick={() => deleteTodo(elem)} className="bg-red-600 text-white p-1 md:p-2  text-[7px] md:text-xs rounded-lg"> DELETE</button>
             </div>
             </motion.div>
             </>
