e56201b3daee938bb6a16d21999e8a27cb32cf12
sahildkun <sahilsahoo23@gmail.com>
2023-01-25 22:07:42 +0530

    small responsive issue

--- Todo_App/src/App.jsx
+++ Todo_App/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
       <video src={videoBg2} autoPlay loop muted  className='absolute hidden md:block' id='videoBg'/>
     <div className=" mx-0 md:mx-12  text-center flex flex-col gap-y-4" id='relative'>
      <div>
-      <motion.p className='text-5xl text-white md:text-gray-800  md:text-5xl pt-44 pb-2 font-serif' animate={{ scale:1.1   }} transition={{ease:'easeIn'}}>TODO APP</motion.p>
+      <motion.p className='text-5xl text-white md:text-gray-800  md:text-5xl pt-44 pb-2 font-serif' animate={{   }} transition={{ease:'easeIn'}}>TODO APP</motion.p>
      </div>
      <form className='space-x-3 space-y-2' onSubmit={handleSubmit}>
       <input type="text" 
@@ -84,7 +84,7 @@ function App() {
             <>
             
             <motion.div className='flex flex-auto items-center   justify-between my-5 bg-[#4037376c] px-1 py-[0.5rem] rounded-lg border-2 border-gray-900' animate={{scale:1.4}} transition={{duration:0.5}}>
-            <h1 key={key} className={`text-left text-xs max-w-[5rem] font-mono line-clamp-3 md:max-w-md ${elem.todoStatus ? 'text-gray-500 line-through' : 'text-white'} `}>{elem.todoData}</h1>
+            <h1 key={key} className={`text-left text-xs max-w-[7rem] font-mono line-clamp-2 md:max-w-md ${elem.todoStatus ? 'text-gray-500 line-through' : 'text-white'} `}>{elem.todoData}</h1>
             <div className='space-x-1 md:space-x-4'>
             <button onClick={() => markTodo(key)} className="bg-green-600 text-white  p-1 md:p-2  text-[7px] md:text-xs font-thin rounded-lg">DONE</button>
             <button onClick={() => deleteTodo(elem)} className="bg-red-600 text-white p-1 md:p-2  text-[7px] md:text-xs rounded-lg"> DELETE</button>
